5f6aa4da0ee8616b31e0aceae439fcbe19dd67a8
Namiq Haciyev <namiq886@gmail.com>
2018-02-12 16:53:17 +0400

    Great user alert others

--- server/server.js
+++ server/server.js
@@ -15,9 +15,26 @@ io.on('connection', (socket) => {
         console.log('User was disconnected');
     });
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getDate()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getDate()
+    });
+
     socket.on('createMessage', (message) => {
         console.log(message);
-        io.emit('newMessage', {
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getDate()
+        // });
+        socket.broadcast.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getDate()
